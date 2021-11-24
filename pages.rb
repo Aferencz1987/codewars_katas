7f50c7302bf9be78e1d1de88e492d692ab9920c3
Alex Ferencz <alex.ferencz6@gmail.com>
2021-11-24 11:53:31 -0700

    Pages with correct answer and failed attempts below

--- pages.rb
+++ pages.rb
@@ -1,54 +1,39 @@
 def page_digits(pages)
-  multiplyer = 2
-  base_pages = 9
-  outter_limit = 100
-  total = 9
-  starting_point = 90
-  return pages if pages < 9
-  # return total + ((pages - 9) * multiplyer) if pages > base_pages && pages < outter_limit
+  return pages if pages < 10
   split_number = pages.to_s.split('')
   places = (split_number.count) - 1
-
+  base_num = 9
+  multiplyer = 1
+  total = 0
   places.times do
-    require "pry"; binding.pry
-    if starting_point == 90
-      total += starting_point * multiplyer
-      starting_point = pages - 99
-      multiplyer += 1
-    else
-      total += starting_point * multiplyer
-      starting_point = pages - starting_point
-      multiplyer += 1
-
-      require "pry"; binding.pry
-    end
-    # require "pry"; binding.pry
+    digit_count = base_num * multiplyer
+    total += digit_count
+    base_num = base_num * 10
+    multiplyer += 1
   end
+
+  array = []
+  (multiplyer - 1).times do
+    array.push(9)
+  end
+  pages_with_counted_digits = array.join.to_i
+  uncounted_pages = pages - pages_with_counted_digits
+
+  total += uncounted_pages * (multiplyer)
   total
 end
 
-pages = 1247   #answer is 633
+pages = 12
 p page_digits(pages)
-
-
-# Johnny is working as an intern for a publishing company, and was tasked with cleaning up old code.
-# He is working on a program that determines how many digits are in all of the page numbers
-# in a book with pages from 1 to n (inclusive).
-#
-# For example, a book with 4 pages has 4 digits (one for each page) while a 12-page
-# book has 15 (9 for 1-9, plus 2 each for 10, 11, 12).
-#
-# Johnny's boss, looking to futureproof, demanded that the new program be able to handle books
-# up to 400,000,000,000,000,000 pages.
-
 
 # def page_digits(pages)
 #   # divide it by
-#   # 1-9 1 8
-#   # 10-99 2 89
-#   # 100-999 3 899
-#   # 1000-9999 4 8999
-#   # 10000 -99999 5 89999
+#   # 1 to 9 1 9
+#   # 9 to 99 2 90
+#   # 100 to 999 3 900
+#   # 1000 to 9999 4 9000
+#   # 10000 to 99999 5 90000
+
 #   #
 #   #
 #   number = [8]
@@ -58,9 +43,6 @@ p page_digits(pages)
 #     split_number = pages.to_s.split('')
 #     places = split_number.count
 #     counter +=
-#
-#
-#
 #
 #
 # number = [8]
@@ -83,3 +65,37 @@ p page_digits(pages)
 #
 #   require "pry"; binding.pry
 # end
+
+
+# multiplyer = 2
+# base_pages = 9
+# outter_limit = 100
+# total = 0
+# starting_point = 90
+# return pages if pages < 10
+# return total + ((pages - 9) * multiplyer) if pages > base_pages && pages < outter_limit
+# split_number = pages.to_s.split('')
+# places = (split_number.count) - 1
+#
+# places.times do
+#   if starting_point == 90
+#     total += starting_point * multiplyer
+#     starting_point = pages - 99
+#     multiplyer += 1
+#   else
+#     total += starting_point * multiplyer
+#     starting_point = pages - starting_point
+#     multiplyer += 1
+#   end
+# end
+# total
+
+# Johnny is working as an intern for a publishing company, and was tasked with cleaning up old code.
+# He is working on a program that determines how many digits are in all of the page numbers
+# in a book with pages from 1 to n (inclusive).
+#
+# For example, a book with 4 pages has 4 digits (one for each page) while a 12-page
+# book has 15 (9 for 1-9, plus 2 each for 10, 11, 12).
+#
+# Johnny's boss, looking to futureproof, demanded that the new program be able to handle books
+# up to 400,000,000,000,000,000 pages.
